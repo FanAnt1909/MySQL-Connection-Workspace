8fbd37155a7fd004574ae386075f2e8cf1b60d1c
= <phananhchssbun@gmail.com>
2021-12-10 20:49:58 +0700

    minor adjustment

--- pen.js
+++ pen.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const csv = require('csv-parser');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-const mysql = require('mysql')
-const sql = require('./sql')
+const sleep = require('system-sleep')
+const sql = require('../sql')
 const query = new sql()
 
 
@@ -11,59 +11,86 @@ const query = new sql()
 //     console.log('Saved!');
 //   });
 
-// fs.appendFile('./csv/test.txt', '\nHello World !!!', err =>{
-//     console.log(err);
-// })
+
 
 const bypass_headers = {
-	'Connection': 'close',
+	'Connection': 'keep-alive',
 	'Upgrade-Insecure-Requests': 1,
-	'Accept-Encoding': ['gzip', 'deflate'],
-	'Accept-Language' : ['en-US', 'en', {q:0.9}]
-  } 
+	'Accept-Encoding': 'gzip, deflate',
+	'Accept-Language' : 'en-US,en;q=0.9'
+} 
 
+//header resembling anonymous tabs requests
+const bypass_headers_anonymous = {
+    'Connection': 'close',
+    'Accept-Encoding': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-US,en;q=0.9',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': "Windows",
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': 1,
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
+}
+
+
+//var setup
+
+
+//create write stream
+var writeStream = fs.createWriteStream("../csv/csv_out/javi.eng.csv")
 
 //parse csv file
-fs.createReadStream('./csv/test.txt')
+fs.createReadStream('../csv/output.csv')
+// fs.createReadStream('../csv/csv_in/javi.csv')
 .pipe(csv())
-.on('data', function(data){
+.on('data', async function(data){
+        var word = '';
+        var text = '';
     try {
     //DO * with data
-    // console.log(data)
-    var string = JSON.stringify(data.Mean)
-    var meaning = string.match(/(?<=mean\\":\\").*?(?=\\")/g)
-    // console.log(meaning)
+        // console.log(data)
+        // var meaning = string.match(/(?<=mean\\":\\").*?(?=\\")/g)
+        // console.log(`meaning: ${meaning}`)
+        var json = JSON.stringify(data.Mean)
+        var meaning = json[0].mean
 
-    var word = '';
-    var text = '';
-    //for each definition of a word
-    for (var i = 0; i < meaning.length; i++){
-        //google translate api url
-        word += ' '
-        word += meaning[i];
-        }
-
-        text = encodeURI(word)
-        // console.log(text)
-        url = 'https://clients4.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=vi&q=' + text
-
+        text = encodeURI(meaning)
+        // console.log(`${data.Id}| ${word}: ${text}`)
+        var url = 'https://clients4.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=vi&q=' + text;
         //call api to google trans to check if a string is vnmese or english
-        fetch(url, {method:'get', header: bypass_headers}).then(res =>{
-            // console.log(word)
+
+        fetch(url, {method:'get', headers: bypass_headers_anonymous})
+        .then(res =>{
             return res.json();
         }).then(output =>{
-            console.log(`${output.src} ${data.Id} ${word}`)
-            console.log('___________________________________')
+            // console.log(`output.src: ${output.src} | id: ${data.Id}`)
+            console.log(`${output.src}| ${data.Id}| ${word}: ${text}`)
+
+        // if language detection return eng
+            if(output.src != 'vi'){
+                //write line to output file
+                var def = data.Mean
+                var csv_Mean = def.replace(/"/g, `""`)
+                var csv_line = `${data.Id},${data.Word},${data.Kana},"${csv_Mean}"\n`
+                // fs.appendFile('../csv/csv_out/javi.eng.csv', csv_line, err =>{
+                //     console.log(`err: ${err}`);
+                // })
+                // writeStream.write(csv_line)
+             }
+        }).catch(e =>{
+            console.log(e)
         })
-
+    
+    }catch(e) {
+        console.log(`error: ${e}`)
     }
-
-    catch(e) {
-        console.log(e)
-    }
+    sleep(1000)
+    //DO END
 })
 .on('end',function(){
     //some final operation
 });  
-
-query.show()
--- sql.js
+++ sql.js
@@ -1,46 +1,48 @@
 const mysql = require('mysql')
 
-    
 console.log('sql intergrated')
 
+//MySQL connection settings
 var opt = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "1909",
     database: "random"
     });
-
+//Connect to MySQL
 opt.connect(function(err) {
     if (err) throw err;
     console.log("SQL Connected")
     });      
 
-
-class Mysql_Query {
-    insert(query){
+//class for MySQL query functions    
+class MySQL_Query {
+    //insert items in table
+    insert(table, value){
         //remove line-break from input query
-        var q = query.replace(/\n/g, "")
-        var q2 = q.replace(/\s{2,10}/g, " ")
-        var q3 = q2.replace(/^\s/g, "")
+        // var q = query.replace(/\n/g, "")
+        // var q2 = q.replace(/\s{2,10}/g, " ")
+        // var q3 = q2.replace(/^\s/g, "")
         //open query
-        opt.query(q3, (err, results) => {
+        var q = `insert into ${table} values(${value})`
+        opt.query(q, (err, results) => {
             if (err) throw err;
             console.log(results);
         })   
 
         //close the connection
-        opt.end(function(err) {
-            if (err) {
-                return console.log('error:' + err.message);
-            }
-            console.log('Close the database connection.');
-        })
+        // opt.end(function(err) {
+        //     if (err) {
+        //         return console.log('error:' + err.message);
+        //     }
+        //     console.log('Close the database connection.');
+        // })
 
     }
 
+    //show all of table
     show(){
-        var q = 'select * from javi'
-
+        var q = 'SELECT * FROM RANDOM'
         //open query
         opt.query(q, function(err, results) {
             if (err) throw err;
@@ -56,7 +58,38 @@ class Mysql_Query {
         });
         
     }
+
+    end(){
+        // close the connection
+        opt.end(function(err) {
+            if (err) {
+                return console.log('error:' + err.message);
+            }
+            console.log('Closed the database connection.');
+        })
+    }
+    //get table columns name
+    get_scheme(table){
+        var q = `SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'${table}';`
+        // var q = query.replace(/\n/g, "")
+        // var q2 = q.replace(/\s{2,10}/g, " ")
+        // var q3 = q2.replace(/^\s/g, "") 
+        //open query
+        opt.query(q, (err, results) => {
+            if (err) throw err;
+            console.log(results);
+            console.log('done');
+        })
+
+        //close the connection
+        opt.end( (err) => {
+            if (err) {
+                return console.log('error:' + err.message);
+            }
+        });
+    }
 }
+// const query = new Mysql_Query
 
-module.exports = Mysql_Query
-
+// query.get_scheme('random');
+module.exports = MySQL_Query
