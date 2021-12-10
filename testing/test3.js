1ce07426c3cfa473c47f3809e50fefc6f4eddc00
= <phananhchssbun@gmail.com>
2021-12-10 20:15:29 +0700

    query file for mock editor query

--- testing/test3.js
+++ testing/test3.js
@@ -1,16 +1,17 @@
 const fs = require('fs')
 const csv = require('csv-parser');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-// const sql = require('../sql')
-// const query = new sql()
+const sql = require('../sql')
+const query = new sql()
 
+function log(state){
+  console.log(state)
+}
 
-fs.writeFile('../csv/csv_in/javi.eng.csv', 'Hello content!', function (err) {
-    if (err) throw err;
-    // console.log('Saved!');
-  });
-
-
+// fs.writeFile('../csv/csv_in/javi.eng.csv', 'Id,Word,Kana,Mean\n', function (err) {
+//     if (err) throw err;
+//     // console.log('Saved!');
+//   });
 
 const bypass_headers = {
 	'Connection': 'close',
@@ -19,25 +20,24 @@ const bypass_headers = {
 	'Accept-Language' : ['en-US', 'en', {q:0.9}]
   }
 
+var writeStream = fs.createWriteStream("../csv/csv_out/javi.eng.csv")
 
 //parse csv file
-// fs.createReadStream('../csv/output.csv')
-fs.createReadStream('../csv/csv_out/javi.eng.csv')
+fs.createReadStream('../csv/output.csv')
+// fs.createReadStream('../csv/csv_out/javi.eng.csv')
 .pipe(csv())
-.on('data', function(data){
+.on('data', async function(data){
     try {
-      console.log(typeof data.Mean == 'string');
-      // console.log(data.Id);
-    // console.log(`data: ${data.Mean}`)
-    json = JSON.parse(data.Mean)
-    // console.log(json[0].mean)
-    console.log(json)
+      // console.log(typeof data.Mean == 'string');
+        // console.log(data.Id);
 
-    //DO * with data
-    // console.log(`${data.Id} : ${json[0].mean}`)
-    // var meaning = string.match(/(?<=mean\\":\\").*?(?=\\")/g)
-    
-    //for each definition of a word
+      // console.log(json[0].mean)
+      log(data)
+      //DO * with data  
+      var csv_line = `${data.Id},'${data.Word}','${data.Kana}','${data.Mean}',null`
+      log(`csv_line: ${csv_line}`)
+      await query.insert('mock',csv_line)
+      console.log('query success')
     }
 
     catch(e) {
@@ -45,5 +45,6 @@ fs.createReadStream('../csv/csv_out/javi.eng.csv')
     }
 })
 .on('end',function(){
-    //some final operation
+    console.log('Done!')
+    query.end()
 });  
