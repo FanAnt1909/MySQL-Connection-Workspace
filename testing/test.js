5fea5e7b11e6309cfc82cd070cebd7f63b888f9a
= <phananhchssbun@gmail.com>
2021-12-14 19:45:03 +0700

    now pause Readstream on every data event to avoid running out of data

--- testing/test.js
+++ testing/test.js
@@ -5,10 +5,16 @@ const sleep = require('system-sleep')
 // const sql = require('../sql')
 // const query = new sql()
 
+function log(state){
+    console.log(state)
+}
+
 
 // fs.writeFile('../csv/csv_out/javi.eng.csv', 'Id,Word,Kana,Mean', function (err) {
 //     if (err) throw err;
 //   });
+
+//PARSE CSV FILE => GET DEFINITION => CALL API TO GET TRANSLATION OF ENG DEFINITION => THEN WRITE IT TO OUTPUT
 
 
 const bypass_headers = {
@@ -34,25 +40,27 @@ const bypass_headers_anonymous = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
 }
 
-//var setup
-//
+//path variable
+var test2 = '../csv/test2.txt'
+var output = '../csv/output.csv'
+var javi = '../csv/csv_in/javi.csv'
 
 //create write stream
 var writeStream = fs.createWriteStream("../csv/csv_out/javi.eng.csv")
-
+var readFile = fs.createReadStream(javi)
 //parse csv file
-fs.createReadStream('../csv/test2.txt')
-// fs.createReadStream('../csv/output.csv')
-// fs.createReadStream('../csv/csv_in/javi.csv')
+readFile
 .pipe(csv())
 .on('data', async (data) => {
-        var word = '';
         var text = '';
     try {
+        //PAUSE STREAM
+        readFile.pause()
     //DO * with data
     // console.log(data)
         var json = JSON.parse(data.Mean)
         var concat = ''
+        //concat all meaning of one line into one string separated by |
         for (let i = 0; i < json.length; i++) {
             concat += json[i].mean
             //if its the final word in arr, dont concat '|'
@@ -60,15 +68,15 @@ fs.createReadStream('../csv/test2.txt')
                 concat += '|'
             }
         }
-        console.log(`concated string: ${concat}`) 
+        // console.log(`concated string: ${concat}`) 
 
         //concat multiples meaning of a word                                                                                                                                                                                   
 
         text = encodeURI(concat)
         //call api to google trans to check if a string is vnmese or english
         var url = 'https://clients5.google.com/translate_a/t?client=dict-chrome-ex&sl=auto&tl=vi&q=' + text;
-
-        fetch(url, {method:'get', headers: bypass_headers_anonymous})
+        log('fetch reached')
+        await fetch(url, {method:'get', headers: bypass_headers_anonymous})
         .then(res =>{
             return res.json();
         }).then(output =>{
@@ -78,8 +86,9 @@ fs.createReadStream('../csv/test2.txt')
             // IF language detection return non-vietnamese
             if(output.src != 'vi'){
                 console.log('eng detected')
+
             //CHANGE DEFINITION OF LINE TO VIETNAMESE
-                //split trans res into corrsponding arr
+                //split trans res into corrsponding array
                 var res_arr =  text.split(' | ')
                 for(let i=0; i < res_arr.length; i++){
                     json[i].mean = res_arr[i]
@@ -91,13 +100,15 @@ fs.createReadStream('../csv/test2.txt')
                 var string = JSON.stringify(json)
                 // var line = data.Mean
                     string = string.replace(/"/g, `""`)
-                    data.Kana = kana.replace(/ /g, ', ')
+                    data.Kana = data.Kana.replace(/ /g, ', ')
                 var csv_line = `${data.Id},${data.Word},${data.Kana},"${string}"\n`
                 
                 //write to output file
+                log('condition true')
                 writeStream.write(csv_line)
             }
             else {
+                log('condition false')
                 //write input without correction to file
                 var csv_line = `${data.Id},${data.Word},${data.Kana},"${data.Mean}"\n`
                 writeStream.write(csv_line)
@@ -105,8 +116,15 @@ fs.createReadStream('../csv/test2.txt')
         }).catch(e =>{
             console.log(e)
         })
+        //RESUME STREAM
+        //readFile.resume()
+        setTimeout(function(){
+            readFile.resume()
+        }
+        ,5000);
+
         //pause function
-        sleep(50)
+        // sleep(50)
     }catch(e) {
         console.log(`error: ${e}`)
     }
